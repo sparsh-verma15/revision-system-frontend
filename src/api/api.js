36f0490d1095b83d40249fbfa181e112813e4500
Sparsh Verma <2020eeb1210@iitrpr.ac.in>
2023-08-23 11:56:44 +0530

    added a comment

--- src/api/api.js
+++ src/api/api.js
@@ -3,4 +3,4 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     // baseURL : "http://localhost:8000"
     baseURL:process.env.REACT_APP_BASE_URL
-});
+}); //to set up base url to make request to server
